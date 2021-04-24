1a3e7a2638008441e98cc8b7768dd1b20e2e7f9b
Sam Cutler <sam.cutler@gmail.com>
2021-04-24 13:13:36 -0700

    Fully functional chess board and knight

--- src/BoardSquare.js
+++ src/BoardSquare.js
@@ -1,16 +1,80 @@
 import React from 'react';
 import Square from './Square';
+import { useDrop } from 'react-dnd';
+import { ItemTypes } from './Constants';
+import { canMoveKnight, moveKnight } from './Game';
 
 export default function BoardSquare( { x, y, children } ) {
+
     const black = (x + y) % 2 === 1;
 
+    const [ { isOver, canDrop }, drop] = useDrop(
+        () => ( {
+            accept: ItemTypes.KNIGHT,
+            canDrop: () => canMoveKnight(x, y),
+            drop: () => moveKnight(x, y),
+            collect: (monitor) => ({
+                isOver: !!monitor.isOver(),
+                canDrop: !!monitor.canDrop()
+            })
+        } ),
+        [x, y]
+    )
+
     return (
-
-        <Square
-            black={black}
+        <div
+            ref={drop}
+            style={ {
+                position: 'relative',
+                width: '100%',
+                height: '100%',
+            } }
         >
-            {children}
-        </Square>
-
+            <Square black={black}>
+                {children}
+            </Square>
+            { isOver && !canDrop && (
+                <div
+                    style={ {
+                        position: 'absolute',
+                        top: 0,
+                        left: 0,
+                        height: '100%',
+                        width: '100%',
+                        zIndex: 1,
+                        opacity: 0.5,
+                        backgroundColor: 'red'
+                    } }
+                />
+            )}
+            { !isOver && canDrop && (
+                <div
+                    style={ {
+                        position: 'absolute',
+                        top: 0,
+                        left: 0,
+                        height: '100%',
+                        width: '100%',
+                        zIndex: 1,
+                        opacity: 0.5,
+                        backgroundColor: 'yellow'
+                    } }
+                />
+            )}
+            { isOver && canDrop && (
+                <div
+                    style={ {
+                        position: 'absolute',
+                        top: 0,
+                        left: 0,
+                        height: '100%',
+                        width: '100%',
+                        zIndex: 1,
+                        opacity: 0.8,
+                        backgroundColor: 'green'
+                    } }
+                />
+            )}
+        </div>
     )
 }
